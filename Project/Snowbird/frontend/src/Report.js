4237fb047698851957694fc6365271ff6820a7c7
ArchanaCS <103047900+ArchanaCS@users.noreply.github.com>
2022-06-27 09:57:03 +0530

    commit

--- Project/Snowbird/frontend/src/Report.js
+++ Project/Snowbird/frontend/src/Report.js
@@ -1,75 +1,175 @@
 import Menu from "./Menu";
-import { FaDownload } from "react-icons/fa";
-import { FiRefreshCw } from "react-icons/fi";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FaDownload, FaFlag } from "react-icons/fa";
+import { FiHelpCircle, FiPrinter, FiRefreshCw } from "react-icons/fi";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Header from "./Header";
+import { AiOutlineMail } from "react-icons/ai";
+import { FiUpload } from "react-icons/fi";
+import {HiBookmark} from "react-icons/hi";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 function Report() {
-  const[taskdetails,setTaskDetails]=useState([]);
-  const[user,setUser]=useState([]);
-  const[uid,setUid]=useState("");
-  useEffect(()=>{
-    var url_u="http://localhost:8000/userfetch";
-    var request={};
-    var header={};
-    axios.post(url_u,request,header).then((res)=>{
-      console.log(res.data);
-      var temp=res.data;
-      console.log("temp",temp);
-      setUser(temp);
-    }).catch()
-  },[])
- 
-  function fetchtask()
-  {
-          var url="http://localhost:8000/timesheet_taskfetch";
-          var request={userid:uid};
-          var header={};
-          console.log("request"+JSON.stringify(request));
-          axios.post(url,request,header).then((res)=>{
-            console.log("response"+JSON.stringify(res.data));
-            setTaskDetails(res.data);
-          }).catch()
+  const [taskdetails, setTaskDetails] = useState([]);
+  const [user, setUser] = useState([]);
+  const [uid, setUid] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  useEffect(() => {
+    var url_u = "http://localhost:8000/userfetch";
+    var request = {};
+    var header = {};
+    axios
+      .post(url_u, request, header)
+      .then((res) => {
+        console.log(res.data);
+        var temp = res.data;
+        console.log("temp", temp);
+        setUser(temp);
+      })
+      .catch();
+  }, []);
+
+  function fetchtask() {
+    var url = "http://localhost:8000/timesheet_taskfetch";
+    var request = { userid: uid };
+    var header = {};
+    console.log("request" + JSON.stringify(request));
+    axios
+      .post(url, request, header)
+      .then((res) => {
+        console.log("response" + JSON.stringify(res.data));
+        setTaskDetails(res.data);
+      })
+      .catch();
   }
   return (
     <>
       <div className="outer">
+        {<Header />}
         <div className="secondrow">
-          {/* Side navigation menu */}
-          {<Menu  />}
+          <div className="firstcolumn">
+            <Menu />
+          </div>
           {/* Main outline */}
           <div className="secondcolumn">
-          <div className="report_rowone">
+            <div className="report_secrow">
+              <div className="report_secrow_left">
+                {" "}
+                <label>TimeSheet</label>
+              </div>
+
+              <div className="report_secrow_right">
+                <AiOutlineMail className="timesheet_icons" />
+                <FiUpload className="timesheet_icons" />
+                <FiPrinter className="timesheet_icons" />
+                <FiHelpCircle className="timesheet_icons" />
+              </div>
+            </div>
+            <div className="timesheet_filters">
+              <label className="timesheet_filter_label">Filters Applied</label>
+              <label className="timesheet_users_label">Users</label>
+              <select className="timesheet_select">
+                <option>Anjaly</option>
+                <option>Sushmitha</option>
+                <option>Anitha</option>
+              </select>
+              <div className="date">
+                <label>From Date</label>
+                <DatePicker
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                />
+              </div>
+              <div className="date">
+                <label className="date_label">To Date</label>
+                <DatePicker
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                />
+              </div>
+              </div>
+              <div className="timesheet_row3">
+               
+                <table id="report">
+                  <tr className="mainheading">
+                <th className="timesheet_task">Task</th>
+                <th className="timesheet_actual">Actual From</th>
+                <th>Actual To</th>
+                <th>Priority</th>
+                <th>Status</th>
+                <th>Estimated Hours</th>
+                </tr>
+                <tr>
+                  <th><HiBookmark className="badge"/>Task1</th>
+                  <th>2022-06-12</th>
+                  <th>2022-06-19</th>
+                  <th>High</th>
+                  <th>To-do</th>
+                  <th>08:00:00</th>
+                </tr>
+                <tr>
+                <th><HiBookmark className="badge"/>Task2</th>
+                  <th>2022-06-12</th>
+                  <th>2022-06-19</th>
+                  <th>Medium</th>
+                  <th>In-Progress</th>
+                  <th>04:00:00</th>
+                </tr>
+                <tr>
+                <th><HiBookmark className="badge"/>Task3</th>
+                  <th>2022-06-12</th>
+                  <th>2022-06-19</th>
+                  <th>Low</th>
+                  <th>Review</th>
+                  <th>04:00:00</th>
+                </tr>
+                </table>
+              </div>
+
+
+
+
+
+
+
+
+
+              
+              {/* 
+            
+           */}
+            
+            {/* <div className="report_rowone">
             <label style={{color:'gray'}}>UserName</label>
-            <br></br>
+            <br></br> */}
             {/* <input type="textbox" /> */}
-            <select onChange={(e) => {setUid(e.target.value) }}>
+            {/* <select onChange={(e) => {setUid(e.target.value) }}>
             <option>--options---</option>
-            {user.map((item,index)=>{
-              // <option>--Options----</option>
+            {user.map((item,index)=>{ */}
+            {/* // <option>--Options----</option>
               return<>
                 <option value={item.id}>{item.txtUserName}</option>
                 </>
             })}
              </select>
             <button onClick={fetchtask}>OK</button>
-          </div>
+          </div> */}
 
-          <div className="report_secrow">
+            {/* <div className="report_secrow">
             <h1>TimeSheet</h1>
             <h3>Parameters Passed-<label style={{fontWeight:'100',fontSize:'medium'}}>One:One,Two</label></h3>
             
-          </div>
-
+          </div> */}
+            {/* 
           <div className="icons">
             <FaDownload className="fa_download"  size={25}  ></FaDownload>&nbsp;&nbsp;
-            <FiRefreshCw className="fa_download" size={25}></FiRefreshCw>
+            <FiRefreshCw className="fa_download" size={25}></FiRefreshCw> */}
             {/* <FontAwesomeIcon icon="fa-solid fa-down-to-line" /> */}
-          </div>
+            {/* </div> */}
 
-          <div className="report_third">
-              
-          <table>
+            <div className="report_third">
+              {/* <table>
             <thead>
             <th >Task</th>
              <th>AssignedTo</th>
@@ -92,7 +192,7 @@ function Report() {
                   <td></td>
                 </tr>
                 </>
-              })}
+              })} */}
               {/* <tr>
                 <td>LoginPage</td>  
                 <td>Ajay</td>
@@ -102,10 +202,10 @@ function Report() {
                 <td>2022-05-10</td>
                 <th>45</th>
                 </tr> */}
-               
-            </tbody>
-            </table>
-          </div>
+
+              {/* </tbody>
+            </table> */}
+            </div>
           </div>
         </div>
       </div>
--- Project/Snowbird/frontend/src/SprintBoard.js
+++ Project/Snowbird/frontend/src/SprintBoard.js
@@ -5,6 +5,7 @@ import Header from "./Header";
 import SprintBox from "./SprintBox";
 import { FaAngleDown, FaFlag, FaPlus, FaRegUserCircle, FaUser, FaUserCircle } from "react-icons/fa";
 import { BsThreeDots } from "react-icons/bs";
+import { VscNoNewline } from "react-icons/vsc";
 function SprintBoard() {
   const [sprintlist, setSprintList] = useState([]);
   const [state, setState] = useState("");
@@ -32,9 +33,10 @@ function SprintBoard() {
   const checkChange = () => {
     setState("checked");
   };
-  function showlist()
+  function showlist(e)
   {
-
+      console.log("e",e)
+        // e.isClicked?"list":"none";
   }
   return (
     <>
@@ -62,7 +64,7 @@ function SprintBoard() {
                 <FaSearch className="fasearch" />
               </div> */}
             </div>
-            <div className="sprint_secondcolumn_row4" onClick={showlist}>
+            <div className="sprint_secondcolumn_row4" onClick={(e)=>{showlist(e.target.value)}}>
               <div className="sprintcolumnrow4_top">
                 <div className="sprintcolumnrow4_top_right">
                   <FaAngleDown />
@@ -103,9 +105,11 @@ function SprintBoard() {
                 </div>
                 </div>
                 
+              
            
                        
               </div>
+             
                 <div className="sprint_secondcolumn_row4_createnew">
                    <FaPlus  className="craetenew_icon"/><label>Create issue</label>
                 </div>
@@ -121,7 +125,7 @@ function SprintBoard() {
             </div>
             <div className="sprint_secondcolumn_row5_right">
               <div className="backlog_right">
-            <div className="dot">3</div>
+            <div className="dot">0</div>
             <button > Create Sprint</button>
             </div>
             </div>
